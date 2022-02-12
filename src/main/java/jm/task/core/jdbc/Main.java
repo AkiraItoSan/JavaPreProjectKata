83ade80400daa73cf4d4177b6066c4775fc05224
AkiraItoSan <akiraitosan99@gmail.com>
2022-02-12 18:34:07 +0700

    UserDaoJDBCImpl: saveUser removeUserById getAllUsers cleanUsersTable User: toString Main: Сделал все по заданию

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,31 @@
 package jm.task.core.jdbc;
 
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
+import java.util.List;
+
 public class Main {
     public static void main(String args[]) {
-//        UserServiceImpl userService = new UserServiceImpl();
-//        userService.createUsersTable();
-//        userService.dropUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+//        Создание таблицы User(ов)
+        userService.createUsersTable();
+//        Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль
+//( User с именем – name добавлен в базу данных )
+
+        userService.saveUser("1", "1-ый", (byte) 1);
+        userService.saveUser("2", "2-ой", (byte) 2);
+        userService.saveUser("3", "3-ий", (byte) 3);
+        userService.saveUser("4", "4-ый", (byte) 4);
+
+//        Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+        userService.getAllUsers().forEach(System.out::println);
+
+//        Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+//        Удаление таблицы
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc.dao;
 
+import com.sun.xml.bind.v2.TODO;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -16,10 +17,10 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Connection con = Util.connectToDataBase(); Statement statement = con.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF not EXISTS users_table " +
-                    "(id INTEGER not NULL, " +
+                    "(id INTEGER not NULL AUTO_INCREMENT, " +
                     " name VARCHAR(50), " +
                     " lastName VARCHAR(50), " +
-                    " age INTEGER not NULL," +
+                    " age TINYINT UNSIGNED not NULL," +
                     " PRIMARY KEY (id))");
 
         } catch (SQLException e) {
@@ -37,18 +38,48 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection con = Util.connectToDataBase(); Statement statement = con.createStatement()) {
+            statement.executeUpdate("INSERT INTO kataschemas.users_table (name, lastName, age) \n" +
+                    "VALUES ('" + name + "','" + lastName + "','" + age + "')");
 
+            System.out.println("User с именем – " + name + " добавлен в базу данных ");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection con = Util.connectToDataBase(); Statement statement = con.createStatement()) {
+            statement.executeUpdate("DELETE FROM kataschemas.users_table\n" +
+                    "     WHERE id='" + id + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try (Connection con = Util.connectToDataBase(); Statement statement = con.createStatement()) {
+            statement.executeQuery("SELECT * FROM kataschemas.users_table");
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                User user = new User(rs.getString("name"), rs.getString("lastname"),
+                        Byte.parseByte(rs.getString("age")));
+                user.setId(Long.parseLong(rs.getString("id")));
+
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection con = Util.connectToDataBase(); Statement statement = con.createStatement()) {
+            statement.executeUpdate("DELETE FROM kataschemas.users_table");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
